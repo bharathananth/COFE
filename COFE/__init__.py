12e0085f21a8157b5ec60d578d519dfe0297c054
Bharath HU <bharath.ananthasubramaniam@hu-berlin>
2025-10-23 14:25:46 +0200

    Exported the necessary public functions so that users do not have to import each submodule separately.

--- COFE/__init__.py
+++ COFE/__init__.py
@@ -2,4 +2,11 @@
 
 __author__ = """Bharath Ananthasubramaniam"""
 __email__ = 'bharath.ananthasubramaniam@hu-berlin.de'
-__version__ = '0.1.0'
+__version__ = '1.3.0'
+
+__all__ = ["preprocess_data", "cross_validate", "plot_markers", "print_markers",
+           "estimate_phase", "plot_diagnostics", "plot_cv_run", "plot_circular_ordering"]
+
+
+from COFE.analyse import preprocess_data, cross_validate, estimate_phase
+from COFE.plot import plot_circular_ordering, plot_cv_run, plot_diagnostics, plot_markers, print_markers
